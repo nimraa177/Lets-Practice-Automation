e6e00c64a8a5eceb0fa58c7ea8fede5e681a3110
Nimra Naseem Awan <58563145+nimraa177@users.noreply.github.com>
2024-07-25 05:24:06 -0500

    added comments

--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -1,9 +1,12 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("https://www.automationexercise.com/login"); //Launching Application
-    cy.title().should("eq", "Automation Exercise - Signup / Login"); //verifying page title
-    cy.get("input[data-qa='login-email']").type("user@admin.com"); //added Email field CSS selector
-    cy.get("input[placeholder='Password']").type("12345"); //added password field CSS selector
-    cy.get("button[data-qa='login-button']").click(); //click on Login button
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    ); //Launching Application
+    cy.title().should("eq", "OrangeHRM"); //verifying page title
+    cy.get("input[placeholder='Username']").type("Admin"); //added Email field CSS selector
+    cy.get("input[placeholder='Password']").type("admin123"); //added password field CSS selector
+    cy.get("button[type='submit']").click(); //click on Login button
   });
+  it
 });
