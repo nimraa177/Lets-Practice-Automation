5a9ee3e0f0ef72fefb5c3d1e30a58e0978cfd8f2
Nimra Naseem Awan <nimra66awan@gmail.com>
2024-08-13 06:13:33 +0500

    added login test cases

--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -1,12 +1,29 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("User Login Functionality", () => {
+  it("should navigate to the login page", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     ); //Launching Application
-    cy.title().should("eq", "OrangeHRM"); //verifying page title
-    cy.get("input[placeholder='Username']").type("Admin"); //added Email field CSS selector
-    cy.get("input[placeholder='Password']").type("admin123"); //added password field CSS selector
-    cy.get("button[type='submit']").click(); //click on Login button
+    cy.title().should("eq", "OrangeHRM"); //verifying page title using should assertion
   });
-  it
+  it("should display an error for incorrect login(Pass)", () => {
+
+    cy.get("input[placeholder='Username']", { timeout: 10000 }).should('be.visible');
+    cy.get("input[placeholder='Username']").type("Admin"); //added Email field CSS selector
+    cy.get("input[placeholder='Password']").type("admin1234"); //added password field CSS selector
+    cy.get("button[type='submit']").click(); //click on Login button
+    cy.get(".oxd-alert-content.oxd-alert-content--error").should('contain', "Invalid credentials"); //verifying Invalid error message
+  });
+
+  // it("should display an error for incorrect login(Email)", () => {
+  //   cy.get("input[placeholder='Username']").type("Adminn"); //added Email field CSS selector
+  //   cy.get("input[placeholder='Password']").type("admin123"); //added password field CSS selector
+  //   cy.get("button[type='submit']").click(); //click on Login button
+  //   cy.get(".oxd-text.oxd-text--p.oxd-alert-content-text").should('contain', "Invalid credentials"); //verifying Invalid error message
+  // });
+  // it("validCredentails", () => {
+  //   cy.get("input[placeholder='Username']").type("Admin"); //added Email field CSS selector
+  //   cy.get("input[placeholder='Password']").type("admin123"); //added password field CSS selector
+  //   cy.get("button[type='submit']").click(); //click on Login button
+  // })
 });
+
